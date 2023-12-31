9ced474c647586bd48c946dc38dcb23d82e0d3cd
Shrestha Shashank <97455610+Shresth72@users.noreply.github.com>
2023-12-31 18:54:48 +0530

    stage 2 header

--- src/main.rs
+++ src/main.rs
@@ -71,8 +71,74 @@ use std::net::{SocketAddr, UdpSocket};
     +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
 */
 
-mod sections;
-use sections::header::*;
+#[derive(Debug, Clone)]
+struct MessageHeader {
+    id: u16,
+    qr: QueryResponseIndicator,
+    op: OpCode,
+    aa: bool,
+    tc: bool,
+    rd: bool,
+    ra: bool,
+    z: u8,
+    rcode: u8,
+    qdcount: u16,
+    ancount: u16,
+    nscount: u16,
+    arcount: u16,
+}
+
+impl MessageHeader {
+    fn to_bytes(&self) -> [u8; 12] {
+        let mut bytes = [0; 12];
+
+        bytes[0] = (self.id >> 8) as u8;
+        bytes[1] = self.id as u8;
+
+        bytes[2] = match self.qr { // Query Response Indicator
+            QueryResponseIndicator::Query => 0,
+            QueryResponseIndicator::Response => 1,
+        } << 7 // shift left 7 bits to get the first bit (MSB)
+            | (self.op as u8) << 3
+            | (self.aa as u8) << 2
+            | (self.tc as u8) << 1
+            | (self.rd as u8);
+
+        bytes[3] = (self.ra as u8) << 7 | self.z << 4 | self.rcode;
+
+        bytes[4] = (self.qdcount >> 8) as u8;
+        bytes[5] = self.qdcount as u8;
+
+        bytes[6] = (self.ancount >> 8) as u8;
+        bytes[7] = self.ancount as u8;
+
+        bytes[8] = (self.nscount >> 8) as u8;
+        bytes[9] = self.nscount as u8;
+
+        bytes[10] = (self.arcount >> 8) as u8;
+        bytes[11] = self.arcount as u8;
+
+        bytes
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Message {
+    header: MessageHeader,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum QueryResponseIndicator {
+    Query,
+    Response,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum OpCode {
+    Query, 
+    InverseQuery,
+    Status,
+}
 
 fn main() -> anyhow::Result<()> {
     let addr = "127.0.0.1:2053";
